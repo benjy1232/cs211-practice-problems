76be9fdfeb59fed601e8b68dbaa5198ff5cc66a3
benjy1232 <b.serrano8970@gmail.com>
2021-09-02 10:10:22 -0500

    Added Comments to Problem 18

--- Problem18/Source.cpp
+++ Problem18/Source.cpp
@@ -7,9 +7,9 @@ int main(void) {
 	cin >> N1;
 	cout << "Please enter second number: ";
 	cin >> N2;
-	for (int i = 0; i < N2; i++) {
-		N3 = N1 * N3;
+	for (int i = 0; i < N2; i++) { //Loops for as many times as the N2, the thing N1 is being raised to
+		N3 = N1 * N3; //N1 is multiplying itself multiple times for everything greater than N1^1
 	}
-	cout << "Result of " << N1 << " raised to the power of " << N2 << ":" << N3;
+	cout << "Result of " << N1 << " raised to the power of " << N2 << ":" << N3; //Outputs the final result
 	return 0;
 }
