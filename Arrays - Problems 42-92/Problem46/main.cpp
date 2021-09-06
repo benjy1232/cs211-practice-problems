32cf6e6ad327ed0db214f86dd55ba5b98b3acdbd
benjy1232 <b.serrano8970@gmail.com>
2021-09-06 17:53:56 -0500

    Problem46

---
+++ Arrays - Problems 42-92/Problem46/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+	int x, i=0;
+	cout << "Please enter integer: ";
+	cin >> x;
+	double doubx=x;
+	for(i=0; doubx>1; i++){
+		doubx=doubx/10;
+	}
+	int arr[i+1];
+	for(int j=0; j<i; j++){
+		doubx=doubx*10;
+		int y=doubx;
+		y=y%10;
+		arr[j]=y;
+	}
+	for(int k=0; k<i; k++){
+		if(k==0)
+			cout << "[" << arr[k] << ", ";
+		else if(k<i-1)
+			cout << arr[k] << ", ";
+		else
+			cout << arr[k] << "]\n";
+	}
+	return 0;
+}
